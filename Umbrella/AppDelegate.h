bf34f4ca6df9af7316d306130a1b1b70de90e31d
DawnWang <wangsguang@gmail.com>
2013-06-14 11:41:08 +0800

    …….

---
+++ Umbrella/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Umbrella
+//
+//  Created by Wang Shuguang on 13-6-14.
+//  Copyright (c) 2013年 Dawn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Umbrella/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Umbrella
+//
+//  Created by Wang Shuguang on 13-6-14.
+//  Copyright (c) 2013年 Dawn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
